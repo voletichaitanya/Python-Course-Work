f3081eebc53cf6373a38eee37da63250c556d4df
Voleti Chaitanya <chaituvoleti6300@gmail.com>
2025-07-10 15:58:32 +0530

    hell

---
+++ classwork/variables.py
@@ -0,0 +1,32 @@
+# Declaring variables and printing
+
+product = "data"
+price = 4500
+stock = True
+print(product, price, stock)  # output : data 4500 True
+
+# Assigining multiple variables
+
+a, b, c = 10, 20, 30
+print(a, b, c)              # Output: 10 20 30
+
+d = e = f = 10                # Assigning same values to multiple variables
+print(d, e, f)              # output 10 10 10
+
+# Updating the value
+
+x = 5
+x = 10
+print(x)                      #  Output: 10
+
+# Swapping among the variables
+
+a, b = 5, 10
+a, b = b, a
+print(a, b)                   # Output: 10 5
+
+
+# Deleting variable
+
+y = 100
+del y                         #

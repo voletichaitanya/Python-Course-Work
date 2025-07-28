4f6b11b0d91a16d183eb163ba9d343d7efe64797
Voleti Chaitanya <chaituvoleti6300@gmail.com>
2025-07-28 11:34:44 +0530

    Hello

--- Assignments/cart.py
+++ Assignments/cart.py
@@ -1,21 +1,8 @@
-data = {
-    1: {'name': 'bananas', 'price': 40},
-    2: {'name': 'apples', 'price': 120},
-    3: {'name': 'rice', 'price': 60},
-    4: {'name': 'sugar', 'price': 45},
-    5: {'name': 'corn', 'price': 30},
-    6: {'name': 'tomato', 'price': 25},
-    7: {'name': 'eggs', 'price': 70},
-    8: {'name': 'onions', 'price': 50},
-    9: {'name': 'pasta', 'price': 90},
-    10: {'name': 'maggie', 'price': 15}
-}
+def generate_sorted_list(n):
+    if n == 1:
+        return [1]
+    else:
+        return generate_sorted_list(n - 1) + [n]
 
-# Print table heading
-print("Index\tItem\t\tPrice")
-print("-" * 30)
-
-# Print each row
-for key, item in data.items():
-    print(f"{key}\t{item['name']}\t\t₹{item['price']}")
-print('-'*30)
+numbers = generate_sorted_list(12)
+print(numbers)
